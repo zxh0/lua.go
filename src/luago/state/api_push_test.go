6fac2e92e5998364ae42a6e5bfae87b1fd37e2a2
zxh0 <zhangxiuhong2013@gmail.com>
2018-12-25 14:17:53 +0800

    fix newGoClosure()

---
+++ src/luago/state/api_push_test.go
@@ -0,0 +1,24 @@
+package state
+
+import "testing"
+import . "luago/api"
+
+func TestPushGoClosure(t *testing.T) {
+	ls := New()
+	ls.PushInteger(1)
+	ls.PushInteger(2)
+	ls.PushInteger(3)
+	ls.PushGoClosure(func(ls LuaState) int {
+		if ls.CheckInteger(LUA_REGISTRYINDEX-1) != 1 {
+			t.Fail()
+		}
+		if ls.CheckInteger(LUA_REGISTRYINDEX-2) != 2 {
+			t.Fail()
+		}
+		if ls.CheckInteger(LUA_REGISTRYINDEX-3) != 3 {
+			t.Fail()
+		}
+		return 0
+	}, 3)
+	ls.Call(0, 0)
+}
