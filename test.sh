2eaeb8c0effc8521aff24564068a6d5536fe7902
zxh0 <zhangxiuhong2013@gmail.com>
2019-04-11 14:35:18 +0800

    fix test.sh

--- test.sh
+++ test.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 set -ex
 
-go install luago/cmd/lua
-./bin/lua ./test/lua-5.3.4-tests/attrib.lua     | grep -q OK
-./bin/lua ./test/lua-5.3.4-tests/bitwise.lua    | grep -q OK
-./bin/lua ./test/lua-5.3.4-tests/calls.lua      | grep -q OK
-./bin/lua ./test/lua-5.3.4-tests/closure.lua    | grep -q OK
-./bin/lua ./test/lua-5.3.4-tests/constructs.lua | grep -q OK
-./bin/lua ./test/lua-5.3.4-tests/events.lua     | grep -q OK
-./bin/lua ./test/lua-5.3.4-tests/goto.lua       | grep -q OK
-./bin/lua ./test/lua-5.3.4-tests/locals.lua     | grep -q OK
-./bin/lua ./test/lua-5.3.4-tests/nextvar.lua    | grep -q OK
-./bin/lua ./test/lua-5.3.4-tests/math.lua       | grep -q OK
-./bin/lua ./test/lua-5.3.4-tests/sort.lua       | grep -q OK
-./bin/lua ./test/lua-5.3.4-tests/strings.lua    | grep -q OK
-./bin/lua ./test/lua-5.3.4-tests/utf8.lua       | grep -q ok
-./bin/lua ./test/lua-5.3.4-tests/vararg.lua     | grep -q OK
-./bin/lua ./test/lua-5.3.4-tests/verybig.lua    | grep -q OK
+go build github.com/zxh0/lua.go/cmd/lua
+./lua ./test/lua-5.3.4-tests/attrib.lua     | grep -q OK
+./lua ./test/lua-5.3.4-tests/bitwise.lua    | grep -q OK
+./lua ./test/lua-5.3.4-tests/calls.lua      | grep -q OK
+./lua ./test/lua-5.3.4-tests/closure.lua    | grep -q OK
+./lua ./test/lua-5.3.4-tests/constructs.lua | grep -q OK
+./lua ./test/lua-5.3.4-tests/events.lua     | grep -q OK
+./lua ./test/lua-5.3.4-tests/goto.lua       | grep -q OK
+./lua ./test/lua-5.3.4-tests/locals.lua     | grep -q OK
+./lua ./test/lua-5.3.4-tests/nextvar.lua    | grep -q OK
+./lua ./test/lua-5.3.4-tests/math.lua       | grep -q OK
+./lua ./test/lua-5.3.4-tests/sort.lua       | grep -q OK
+./lua ./test/lua-5.3.4-tests/strings.lua    | grep -q OK
+./lua ./test/lua-5.3.4-tests/utf8.lua       | grep -q ok
+./lua ./test/lua-5.3.4-tests/vararg.lua     | grep -q OK
+./lua ./test/lua-5.3.4-tests/verybig.lua    | grep -q OK
+echo "OK"
